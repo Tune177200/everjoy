b87d6d3f050fa1f4edbb54b5082bc89c40867bd8
vubinh127 <81604514+vubinh127@users.noreply.github.com>
2024-01-09 17:11:42 +0700

    Add files via upload

--- html/assets/js/custom_swiper.js
+++ html/assets/js/custom_swiper.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".main_slide", {
     pagination: {
         el: ".swiper-pagination",
     },
@@ -6,4 +6,24 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    loop: true,
+});
+
+var swiper = new Swiper(".list_slide", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true,
+    slidesPerView: 4,
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    }
 });
