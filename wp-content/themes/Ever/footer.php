816531340133b5e44229881ac1422dba628df208
vubinh12720 <binhvl@newwave.vn>
2024-01-12 02:01:33 +0700

    html footer

--- wp-content/themes/Ever/footer.php
+++ wp-content/themes/Ever/footer.php
@@ -1,4 +1,16 @@
 <?php wp_footer(); ?>
 </main>
+<footer>
+    <div class="bottom_nav">
+        <div class="d-flex align-items-center">
+            <div>
+                <a href="">
+                    <img src="" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+
+</footer>
 </body>
 </html>
